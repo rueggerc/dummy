8f1e9c8edf109280493e3cf73f0c967a4b471abd
Chris <chris.ruegger@gmail.com>
2017-11-24 06:34:24 -0500

    Added Tests

---
+++ DummyProject/src/com/rueggerllc/tests/CoreTests.java
@@ -0,0 +1,47 @@
+package com.rueggerllc.tests;
+
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class CoreTests {
+
+	private static Logger logger = Logger.getLogger(CoreTests.class);
+	
+	
+	
+	@BeforeClass
+	public static void setupClass() throws Exception {
+	}
+	
+	@AfterClass
+	public static void tearDownClass() throws Exception {
+	}
+
+	@Before
+	public void setupTest() throws Exception {
+	}
+
+	@After
+	public void tearDownTest() throws Exception {
+	}
+	
+	@Test
+	@Ignore
+	public void dummyTest() {
+		logger.info("Inside Dummy Test");
+	}
+	
+	
+	@Test
+	@Ignore
+	public void testAnagrams() {
+	}
+	
+	
+	
+}
